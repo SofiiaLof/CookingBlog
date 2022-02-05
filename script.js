17f58639bc1b7af856142b751e545cead9b8f911
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-02-05 16:43:13 +0100

    Kopplade bootstrap-icons, stylade posts, fixade layout

--- script.js
+++ script.js
@@ -7,37 +7,48 @@ function printPosts(){
     const sectionPosts = document.getElementById("posts-list");
     sectionPosts.innerHTML = " ";
 
+    const row = document.createElement("div");
+    row.className = "row justify-content-center";
+    sectionPosts.append(row);
+    
+
     for(let i = 0; i < post.posts.length; i++){
         const item = post.posts[i];
 
-        const divElement = document.createElement("div");
-        divElement.className = "post-items";
+        const card = document.createElement("div");
+        card.className = "post-items p-3 m-3 col-8";
 
-        const liElement = document.createElement("li");
-        liElement.className = "post-item";
+        const cardBody = document.createElement("div");
+        cardBody.className = "post-item card-body";
 
        const title = document.createElement("h2");
        const image = document.createElement("img");
-       image.className ="post-image";
+       image.className ="post-image card-img-top";
 
        const content = document.createElement("p");
        const date = document.createElement("span");
-       const deleteBtn = document.createElement("button");
-       deleteBtn.innerHTML = "Delete";
+       const divButton = document.createElement("div");
+       divButton.className = "d-flex justify-content-end";
 
+       const deleteBtn = document.createElement("button");
+       deleteBtn.className = "deleteBtn";
+       const deleteIcon = document.createElement("i");
+       deleteIcon.className = "bi bi-x-square";
+      
        title.textContent = item.title;
        image.src = item.image;
        content.textContent = item.content;
        date.textContent = item.date;
 
-       liElement.append(image);
-       liElement.append(title);
-       liElement.append(content);
-       liElement.append(date);
-       liElement.append(deleteBtn);
-
-        divElement.append(liElement);
-        sectionPosts.append(divElement);
+        card.append(image);
+        cardBody.append(title);
+        cardBody.append(content);
+        cardBody.append(date);
+        card.append(cardBody);
+        card.append(divButton);
+        divButton.append(deleteBtn);
+        deleteBtn.append(deleteIcon);
+        row.append(card);
     }
 
 }
