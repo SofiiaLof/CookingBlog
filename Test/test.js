b6aeb354c01dcb1c9ef15d96bd9c34e8f102df53
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-02-13 00:24:39 +0100

    Fixade testsuite till metoden som tar bort en post objekt, samt städade lite

--- Test/test.js
+++ Test/test.js
@@ -17,20 +17,45 @@ describe("AddNewPost", function (){
         const image = "";
         const content = "Something about cooking";
     
-        console.log(Object.entries(localStorage));
         const contentForStorage = localStorage.stashContent();
       
-        console.log(Object.entries(localStorage));
-        console.log(contentForStorage);
-
         post.addNewPost(title, image, content);
         const lastObjectinArray = post.posts[post.posts.length-1].title;
-        console.log(lastObjectinArray);
+ 
         assert.equal(lastObjectinArray, title);
 
         localStorage.restoreContent(contentForStorage);
-        console.log(Object.entries(localStorage));
+
        
     });
 });
 
+describe("deletePost", function (){
+    it("ta bort en post objekt", function (){
+
+        const post = new Post();
+
+        const title = "Post2";
+        const image = "";
+        const content = "Something about cooking";
+
+       
+        const contentForStorage = localStorage.stashContent();
+        const lengthBefore  = post.posts.length;
+
+        post.addNewPost(title, image, content);
+       
+        const lastObjectinArray = (post.posts.length - 1);
+
+        post.deletePost(lastObjectinArray);
+
+        const lengthAfter = post.posts.length;
+
+        localStorage.restoreContent(contentForStorage);
+    
+        assert.equal(lengthBefore, lengthAfter);
+
+     
+
+    });
+});
