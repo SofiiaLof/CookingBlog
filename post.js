61c685cf636e760038b65b24b23741742c8e646c
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-02-13 19:18:48 +0100

    Fixade jsdoc kommentarer

--- post.js
+++ post.js
@@ -1,5 +1,13 @@
+/**
+ * A class representing a blog post
+ * @class  
+ */
 export class Post {
   
+  /**
+   * 
+   *Gets post array from localStorage if not null, creates an empty object array otherwise
+   */
   constructor() {
 
     if(this.openPostsFromStorage() === null){
@@ -14,6 +22,12 @@ export class Post {
 
   }
 
+  /**
+   * Adds new post object to the post-array and saves new post-array to the localStorage with a @method savePostsToStorage.
+   * @param {string} title -The title to be added
+   * @param {string} content -The content to be added
+   * @param {string} image - The image to be added
+   */
   addNewPost(title, content, image) {
     let date = new Date();
     let day = date.getDate();
@@ -21,7 +35,6 @@ export class Post {
     let year = date.getFullYear();
     let fullDate = `${year}/${month}/${day}`;
   
-    
     const newPost = {
       title: title,
       image: image,
@@ -32,21 +45,31 @@ export class Post {
   
     this.posts.push(newPost);
     this.savePostsToStorage();
-    
+   
   }
-
+  
+  /**
+   * Deletes a chosen post object from storage array @method openPostsFromStorage
+   * @param {string} chosenPost - The id of post to be deleted 
+   */
   deletePost(chosenPost) {
-    
     let storageArray = this.openPostsFromStorage();
     storageArray.splice(chosenPost,1);
     this.posts = storageArray;
     this.savePostsToStorage();
   }
 
+  /**
+   * Saves array with post objects to localStorage
+   */
   savePostsToStorage() {
     window.localStorage.setItem("posts", JSON.stringify(this.posts));
   }
 
+/**
+ * Fetch post array from localStorage
+ * @returns {array|null}
+ */
   openPostsFromStorage() {
     const storage = JSON.parse(window.localStorage.getItem("posts"));
     return storage;
